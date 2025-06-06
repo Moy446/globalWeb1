d4a8be5255c91cbfc0d5190879aa5f3ffc34d842
Moy446 <a22310402@ceti.mx>
2025-06-05 23:12:29 -0600

    base de datos azure

--- php/conexionPrincipal.php
+++ php/conexionPrincipal.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = mysqli_connect("localhost","root","") or die ("Error en la conexion de la base de datos ");
+    $conexion = mysqli_connect("server-dbtecnology.mysql.database.azure.com","olqrmykmdb","4wT2BL$kVAPmiH96") or die ("Error en la conexion de la base de datos ");
     mysqli_select_db($conexion,"bdtecnology") or die ("Error en la base de datos");
 ?>
