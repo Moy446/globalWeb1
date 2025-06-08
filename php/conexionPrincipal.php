e922da27c1b37aaaee7fe1b0746fae69d115bc8f
Moy446 <a22310402@ceti.mx>
2025-06-07 22:28:37 -0600

    prueba 2 azure

--- php/conexionPrincipal.php
+++ php/conexionPrincipal.php
@@ -1,3 +1,4 @@
 <?php
     $con = mysqli_init();
+    mysqli_ssl_set($con,NULL,NULL, "../BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
     mysqli_real_connect($conn, "server-dbtecnolgoy.mysql.database.azure.com", "dbtecnology", "@2025drako", "bdtecnology", 3306, MYSQLI_CLIENT_SSL);
