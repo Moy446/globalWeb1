daecf6620ef0740ba8c0c4f3143588ae04754857
Moy446 <a22310402@ceti.mx>
2025-06-07 22:34:07 -0600

    prueba 3

--- php/conexionPrincipal.php
+++ php/conexionPrincipal.php
@@ -1,4 +1,4 @@
 <?php
     $con = mysqli_init();
     mysqli_ssl_set($con,NULL,NULL, "../BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-    mysqli_real_connect($conn, "server-dbtecnolgoy.mysql.database.azure.com", "dbtecnology", "@2025drako", "bdtecnology", 3306, MYSQLI_CLIENT_SSL);
+    mysqli_real_connect($con, "server-dbtecnolgoy.mysql.database.azure.com", "dbtecnology", "@2025drako", "bdtecnology", 3306, MYSQLI_CLIENT_SSL);
